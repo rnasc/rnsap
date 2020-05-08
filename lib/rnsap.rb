285dc86ebf634f855f14dbd71f08f536cd8c255e
Rogerio Nascimento <rogerio@rogerionascimento.com>
2020-05-08 17:08:18 +0100

    Adding yard documentation

--- lib/rnsap.rb
+++ lib/rnsap.rb
@@ -4,35 +4,59 @@ require 'nwrfc'
 
 include NWRFC
 
+# Module for SAP helper methods. RnSap allows for a simpler
+# manner to access SAP servers calling RFC BAPIs.
+# @author Rogerio Nascimento
 module RnSap
+  # This is the central class responsible for SAP remote function calls.
+  # Currently the RFC_READ_TABLE BAPI is implemented.
+  #
+  # * TODO
+  #
+  # Implement other SAP BAPIs, such as BAPI_GOODS_MOVEMENT.
   class Sap
+    # keeps the SAP connection alive during the Sap instance lifecycle
     attr_reader :conn
 
     # Constructor requires to receive connection parameters,
     # a hash containint necessary information to logon to SAP
-    def initialize(_conn_parms)
-      @conn = Connection.new(_conn_parms)
+    # @param conn_parms [Hash] SAP Connection Parameters
+    def initialize(conn_parms)
+      @conn = Connection.new(conn_parms)
     end
 
-    # Closes SAP connection
+    # Closes the instance's SAP connection#
     def close
       conn.disconnect
     end
 
-    # def read_table(params = {})
-    #   return [] unless params[:name].present?
-    #   return [] unless params[:fields].present?
-
-    #   name = params[:name]
-    #   fields = params[:fields]
-    #   clauses = params[:clauses].present? ? params[:clauses] : []
-    #   rowskips = params[:row_skip].present? ? params[:row_skip] : 0
-    #   rowcount = params[:row_count].present? ? params[:row_count] : 0
-
-    #   read_table(name, fields, clauses, rowskips, rowcount)
+    # Invokes SAP RFC_READ_TABLE function module remotely, passing
+    # the table to be read and receiving back a list of objects of that
+    # table.
+    #
+    # Example:
+    #
+    # - name: 'lfa1'
+    # - fields: ['name1', 'land1']
+    #
+    # The return will be an array of object <b>Lfa1</b> with the
+    # attributes <b>name1</b> and <b>land1</b>.
+    #
+    # ```ruby
+    # conn = RnSap::Sap.new(conn_parms)
+    # list = conn.read_table('lfa1', ['name1', 'land1'])
+    # list.each do |item|
+    #    puts item.name1
     # end
-
-    def read_table(name = '', fields = [], clauses = [], _row_skip = 0, _row_count = 0)
+    # ```
+    #
+    # @param name [String] Name of SAP table to be read
+    # @param fields [Array] Array of strings containing the table field names to be retrieved
+    # @param clauses [Array] Optional 'where' clauses applicable to the table reading. respects SAP sintax
+    # @param _row_skip [Integer]  number of rows to be skipped within the selected data
+    # @param _row_count [Integer] max number of rows to be returned
+    # @return [Array] Array of the Objects named after the table (i.e. Lfa1, Mara, Bkpf) with its selected columns as attributes (see example)
+    def read_table(name, fields = [], clauses = [], _row_skip = 0, _row_count = 0)
       if name.is_a?(Hash)
         fields = name[:fields]
         clauses = name[:clauses]
@@ -101,6 +125,7 @@ module RnSap
 
     attr_writer :conn
 
+    # Dumps to the output the content of an object
     def dump_instance_variables(obj)
       puts "Class: #{obj.class} -> #{obj}"
       obj.instance_variables.map do |var|
@@ -108,6 +133,11 @@ module RnSap
       end
     end
 
+    # Dynamically returns a class instance with the name 'name' and with each
+    # of its fields as an attribute
+    # @param name [String] name of the intended class instance
+    # @param fields [Array] array of strings containing the list of attributes to be available in the class instance
+    # @return [Object] instance of the object 'Name'
     def get_class_instance(name, fields)
       # puts "Class name: #{name}"
       klass = Object.const_set(name, Class.new).new # , Struct.new(*attributes)
@@ -118,6 +148,8 @@ module RnSap
     end
   end
 
+  # class used to represent a Table Column. Used internally
+  # to map SAP's returned working area (WA) into class instances
   class TableColumn
     attr_accessor :field_name, :offset, :length, :type, :description
   end
